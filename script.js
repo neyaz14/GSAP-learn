27899a03dedd74b929d2b69501504a9dbf5a6574
neyaz <neyazmorshid@gmail.com>
2025-01-23 17:29:45 +0600

    done

--- script.js
+++ script.js
@@ -75,44 +75,93 @@
 
 // !-->  scrolltriger 
 // ? -> jokhon scroll kora hobe tokhon animation hobe, 
-gsap.from("#page1 #box",{
-    scale:0,
-    duration:1.5,
-    delay:1,
-    rotate:360
-})
-gsap.from("#page2 h1",{
-    scale:0,
-    duration:1.3,
-    // delay:.8,
-    x:500,
-    // rotate:360,
-    scrollTrigger:{
-        trigger:"#page2 h1",
-        scroller:"body",
-        markers:true,
-        start:"top 50%"
-    }
-})
-gsap.from("#page2 h2",{
-    scale:0,
-    duration:1.3,
-    // delay:.8,
-    x:-500,
-    // rotate:360,
-    scrollTrigger:{
-        trigger:"#page2 h2",
-        scroller:"body",
-        markers:true,
-        start:"top 30%"
-    }
-})
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     duration:1.5,
+//     delay:1,
+//     rotate:360
+// })
+// gsap.from("#page2 h1",{
+//     scale:0,
+//     duration:1.3,
+//     // delay:.8,
+//     x:500,
+//     // rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         // scrub --> 
+//         scrub:2
+
+//     }
+// })
+
+//! --> pin to make side scrollign
+// gsap.to('#page2 h1',{
+//     transform:"translateX(-425%)",
+//     scrollTrigger:{
+//         //? pin use korle element er parrent ke trigger korte hobe 
+//         trigger: "#page2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 0%",
+//         end:"top -150%",
+//         scrub:4,pin:true
+//     }
+// })
+
+// gsap.from("#page2 h2",{
+//     scale:0,
+//     duration:1.3,
+//     // delay:.8,
+//     x:-500,
+//     // rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 30%",
+//         scrub:true
+//     }
+// })
 
 
+// gsap.from("#page3 #box",{
+//     scale:0,
+//     duration:1.5,
+//     delay:1,
+//     rotate:360
+// })
+var Path =`M 10 100 Q 500 100 990 100`;
+var finalPath =`M 10 100 Q 500 100 990 100`;
+var string =document.querySelector("#string");
 
-gsap.from("#page3 #box",{
-    scale:0,
-    duration:1.5,
-    delay:1,
-    rotate:360
+// string.addEventListener("mouseenter",function(){
+//     console.log("mouse entered")
+// })
+// string.addEventListener("mouseleave",function(){
+//     console.log("mouse leave")
+// })
+string.addEventListener("mousemove",function(dets){
+    Path=`M 10 100 Q ${dets.x} ${dets.y} 990 100`
+    gsap.to("svg path",{
+        // svg er under e thaka attribute ke change korte -- attr: (then oi attribute er jei value change korbo ta)
+        attr:{
+            d:Path,
+            duration:0.3,
+            ease:"power3.out"
+        }
+    })
+})
+string.addEventListener("mouseleave", function(){
+    gsap.to("svg path",{
+        attr:{d:finalPath,
+            duration:1.5,
+            ease: "bounce.out",
+            // ease: "elastic.out(1,0.1)",
+            y: -20}
+    })
 })
